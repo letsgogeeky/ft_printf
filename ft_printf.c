e123c4410cfbe52abd6800f14fb0a8e9f8abe607
Ramy Moussa <ramoussa@3-E-7.42heilbronn.de>
2023-04-24 23:08:38 +0200

    progress on void * to hex

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,38 @@
 /*   By: ramoussa <ramoussa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 21:44:51 by ramoussa          #+#    #+#             */
-/*   Updated: 2023/04/19 23:37:32 by ramoussa         ###   ########.fr       */
+/*   Updated: 2023/04/24 23:00:21 by ramoussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
+
+int	handle_hex(unsigned long ptr, int to_upper)
+{
+	char	*hex_set;
+	int		len;
+	char	c;
+	hex_set = "0123456789abcdef";
+	// write(1, "0x", 2);
+	// len = 2;
+	len = 0;
+	if (ptr > 0 && ptr <= 15)
+	{
+		c = ptr;
+		len += 1;
+	}
+	else if (ptr > 0)
+	{
+		c = ptr % 16;
+		len += handle_hex(ptr / 16, to_upper);
+	}
+	if (to_upper == 1 && c > 9)
+		write(1, &hex_set[(int)c] - 32, 1);
+	else
+		write(1, &hex_set[(int)c], 1);
+	return (len);
+}
 
 int	type_factory(va_list params, char fmt)
 {
@@ -22,6 +48,8 @@ int	type_factory(va_list params, char fmt)
 	else if (fmt == 'p')
 	{
 		// check how to print a pointer in hexadecimal
+		write(1, "0x", 2);
+		handle_hex((unsigned long)va_arg(params, void *), 0);
 	}
 	else if (fmt == 'd')
 		ft_putnbr_fd(va_arg(params, int), 1);
@@ -36,6 +64,8 @@ int	type_factory(va_list params, char fmt)
 	else if (fmt == 'X')
 	{
 		// print a number in hexadecimal format (uppercase)
+		write(1, "0x", 2);
+		handle_hex((unsigned long)va_arg(params, long), 1);
 	}
 	else if (fmt == '%')
 		ft_putchar_fd('%', 1);
@@ -47,18 +77,35 @@ int	type_factory(va_list params, char fmt)
 int	ft_printf(const char *str, ...)
 {
 	va_list	params;
-	int	first_arg, second_arg, third_arg, fourth_arg;
+	int	i;
+	// int	first_arg, second_arg, third_arg, fourth_arg;
+	
+	if (!str)
+		return (-1);
 	va_start(params, str);
-
-	type_factory(params, 'd');
-	first_arg = va_arg(params, int);
-	printf("FOUND IT!! HERE: %d\n", first_arg);
-	second_arg = va_arg(params, int);
-	printf("WE TRIED TO CAST THIS TO INT: %d\n", second_arg);
-	third_arg = va_arg(params, int);
-	printf("WE TRIED TO CAST ANOTHER CHAR TO INT: %d\n", third_arg);
-	fourth_arg = va_arg(params, int);
-	printf("WE TRIED TO CAST A CHAR ARRAY TO INT HAHAH!! %d\n", fourth_arg);
+	i = 0;
+	while(str && str[i])
+	{
+		if (str[i] == '%' && str[i + 1])
+		{
+			type_factory(params, str[i + 1]);
+			i++;
+		}
+		else
+		{
+			ft_putchar_fd(str[i], 0);
+		}
+		i++;
+	}
+	// type_factory(params, 'd');
+	// first_arg = va_arg(params, int);
+	// printf("FOUND IT!! HERE: %d\n", first_arg);
+	// second_arg = va_arg(params, int);
+	// printf("WE TRIED TO CAST THIS TO INT: %d\n", second_arg);
+	// third_arg = va_arg(params, int);
+	// printf("WE TRIED TO CAST ANOTHER CHAR TO INT: %d\n", third_arg);
+	// fourth_arg = va_arg(params, int);
+	// printf("WE TRIED TO CAST A CHAR ARRAY TO INT HAHAH!! %d\n", fourth_arg);
 	return (0);
 }
 
--- main.c
+++ main.c
@@ -6,13 +6,24 @@
 /*   By: ramoussa <ramoussa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 18:25:30 by ramoussa          #+#    #+#             */
-/*   Updated: 2023/04/19 23:17:14 by ramoussa         ###   ########.fr       */
+/*   Updated: 2023/04/24 22:47:33 by ramoussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
+
+char	get_first_chr(char *str)
+{
+	if (!str)
+		return (0);
+	return str[0];
+}
 
 int	main()
 {
-	ft_printf("Hello World", 421232112, '2', 's', "HEEEEY");
+	void	*f = get_first_chr;
+	ft_printf("H %d el %% lo %c World %c || %s    || %p\n", 421232112, '2', 's', "HEEEEY", f);
+	printf("H %d el %% lo %c World %c || %s    || %p\n", 421232112, '2', 's', "HEEEEY", f);
+	return (0);
 }
